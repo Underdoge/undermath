af5b486453032cb8694177028919bac56a292fcf
Underdoge <eduardo.chapa@gmail.com>
2023-06-01 02:19:04 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup
 
 setup(
     name='undermath',
-    version='1.0.0',
+    version='1.0.3',
+    description='Este paquete contiene módulos para realizar cálculos aritméticos y estadísticos sencillos.',
+    author='Underdoge',
+    author_email='eduardo.chapa@gmail.com',
+    url='https://github.com/Underdoge/undermath',
     install_requires=[
         'argparse',
         'importlib-metadata; python_version == "3.10"',
